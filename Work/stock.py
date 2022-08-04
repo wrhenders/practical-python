faf7a531193165ea5b15cd1a5ee80595e9d7fce3
Ryan Henderson <wrhenders@gmail.com>
2022-08-04 09:19:01 -0500

    Section 5

--- Work/stock.py
+++ Work/stock.py
@@ -1,4 +1,7 @@
 class Stock:
+
+    __slots__ = ('name', '_shares', 'price')
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -7,6 +10,7 @@ class Stock:
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
 
+    @property
     def cost(self):
         return self.shares * self.price
 
@@ -14,3 +18,13 @@ class Stock:
         self.shares -= num
         if self.shares < 0:
             self.shares = 0
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
