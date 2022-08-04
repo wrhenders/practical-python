ebd08ba4133d482470abf79b0b76bcfcd909d3bf
Ryan Henderson <wrhenders@gmail.com>
2022-08-04 11:05:02 -0500

    Section 6

--- Work/report.py
+++ Work/report.py
@@ -4,6 +4,7 @@
 
 import fileparse
 from stock import Stock
+from portfolio import Portfolio
 import tableformat
 
 
@@ -12,10 +13,13 @@ def read_portfolio(filename):
     Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.
     '''
-    with open(filename) as f:
-        file_dictionary = fileparse.parse_csv(
-            f, select=['name', 'shares', 'price'], types=[str, int, float])
-    return [Stock(d['name'], d['shares'], d['price']) for d in file_dictionary]
+    with open(filename) as file:
+        portdicts = fileparse.parse_csv(file,
+                                        select=['name', 'shares', 'price'],
+                                        types=[str, int, float])
+
+    portfolio = [Stock(d['name'], d['shares'], d['price']) for d in portdicts]
+    return Portfolio(portfolio)
 
 
 def read_prices(filename):
--- Work/scrap.py
+++ Work/scrap.py
@@ -1,13 +1,24 @@
-import csv
-f = open('Data/portfolio.csv')
-rows = csv.reader(f)
-next(rows)
-row = next(rows)
+# import csv
+# f = open('Data/portfolio.csv')
+# rows = csv.reader(f)
+# next(rows)
+# row = next(rows)
 
-t = (row[0], int(row[1]), float(row[2]))
+# t = (row[0], int(row[1]), float(row[2]))
 
-d = {
-    'name': row[0],
-    'shares': int(row[1]),
-    'price': float(row[2])
-}
+# d = {
+#     'name': row[0],
+#     'shares': int(row[1]),
+#     'price': float(row[2])
+# }
+
+
+def filematch(filename, substr):
+    with open(filename, 'r') as f:
+        for line in f:
+            if substr in line:
+                yield line
+
+
+for line in filematch('Data/portfolio.csv', 'IBM'):
+    print(line, end='')
